41e93708c748e3b4c447f83df1e13b9df307ffda
root <root@DESKTOP-KGC7C0L>
2023-08-20 23:29:03 +0300

    1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from catalog.views import contacts,home
+
+
 urlpatterns = [
-    path('',contacts),
-    path('',home),
+    path("", contacts),
+    path("catalog/", contacts, name="contacts"),
+    path("home/", home,name="home"),
 ]
+
+
